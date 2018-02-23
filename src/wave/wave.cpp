159d51fbc3b29c1629ff82fdb2ff1f12505ed5be
florence wacheux <fwacheux@rollinier.irisa.fr>
2018-02-23 10:25:36 +0100

    first commit

---
+++ src/wave/wave.cpp
@@ -0,0 +1,10 @@
+#include "wave.hh"
+
+
+
+std::vector<its::State> computePath (const its::ITSModel & model) {
+  std::vector<its::State> v;
+  return v;
+}
+
+
---
+++ src/wave/wave.hh
@@ -0,0 +1,10 @@
+#ifndef __WAVE_HH__
+#define __WAVE_HH__
+
+#include "its/ITSModel.hh"
+
+
+std::vector<its::State> computePath (const its::ITSModel & model);
+
+
+#endif
